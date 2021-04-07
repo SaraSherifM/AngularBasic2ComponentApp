34e468c8645a4901bf46765fc136e9c8a06dd774
sara0sherif <sara0sherif@gmail.com>
2021-04-07 22:16:47 +0200

    modofication on simple form

--- src/app/applicant/applicant.component.ts
+++ src/app/applicant/applicant.component.ts
@@ -1,76 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl,FormGroup ,Validators, FormBuilder, FormArray} from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+  FormArray,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-applicant',
-  templateUrl: './applicant.component.html',
-  styleUrls: ['./applicant.component.css']
+  selector: "app-applicant",
+  templateUrl: "./applicant.component.html",
+  styleUrls: ["./applicant.component.css"],
 })
 export class ApplicantComponent implements OnInit {
+  levelArray: string[] = ["Native", "Fluent", "Very Good", "Good"];
+  constructor(private fb: FormBuilder) {}
 
-  constructor(private fb: FormBuilder) { }
-
-  applicant:any;
+  applicant: any;
   applicantData;
 
-
-  levelArray:string[] = ['Native','Fluent','Very Good','Good'];
-  formgroup;
-  ngOnInit(): void { 
-
+  ngOnInit(): void {
     this.applicantData = this.fb.group({
+      name: ["", Validators.required],
+      email: ["", Validators.required],
+      languages: this.fb.array([this.addLanguageAndLevelFormGroup()]),
+    });
+  }
 
-      name: ['', Validators.required],
-
-      email: ['', Validators.required ],
-
-      languages: this.fb.array([
-        this.addLanguageAndLevelFormGroup()
-      ]),
-
-     
-  });  }
-
-  
-  addLanguageClick():void{
-    //get returns abstract control and not form Array type cast to Formarray
-    
-    (<FormArray>this.applicantData.get('languages')).push(this.addLanguageAndLevelFormGroup());
-
+  addLanguageClick($event): void {
+    $event.preventDefault();
+    this.languages.push(this.addLanguageAndLevelFormGroup());
     console.log(this.languages.controls);
-    console.log(this.applicantData.controls.languages.controls.formgroup)
+    console.log(this.applicantData.controls.languages.controls.formgroup);
     //console.log(`form Array .controls access first element+ ${this.languages.controls['1']} `);
-
-
   }
 
-  get languages(){
-    return this.applicantData.get('languages') as FormArray
+  //get returns abstract control and not form Array type cast to Formarray
+  get languages(): FormArray {
+    return this.applicantData.get("languages") as FormArray;
   }
 
+  addLanguageAndLevelFormGroup(): FormGroup {
+    return this.fb.group({
+      language: [""],
+      level: [""],
+    });
+  }
 
-   addLanguageAndLevelFormGroup( ):FormGroup {
-     return  this.fb.group({
-       language: [''],
-       level: ['']
-     });
-      
-   }
-   
-   counter:number ;
+  counter: number;
 
-   onSubmit(){
+  onSubmit($event) {
+    $event.preventDefault();
+    if (this.applicantData.valid) {
+      this.applicant = this.applicantData.value;
+      console.log();
+      for (let i = 1; i < this.languages.length; i++) {
+        this.languages.removeAt(i);
+      }
 
-    if(this.applicantData.valid)
-    {
-      this.applicant =this.applicantData.value;
-      console.log(this.applicant); 
-      this.applicantData.reset(); 
-      
+      this.applicantData.reset();
     }
+
     this.counter = 0;
-
-   }
-
-    
+  }
 }
